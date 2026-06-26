9a68a6e64340a99b7d8b043cefae9e5cde75cc94
Saulo De Castro Bittencourt <scbittencourt@stefanini.com>
2026-06-26 14:34:08 -0300

    Adicionando o projeto completo

---
+++ src/main/java/com/javanauta/aprendendospring/controller/UsuarioController.java
@@ -0,0 +1,48 @@
+package com.javanauta.aprendendospring.controller;
+
+import com.javanauta.aprendendospring.business.UsuarioService;
+import com.javanauta.aprendendospring.controller.dtos.UsuarioDto;
+import com.javanauta.aprendendospring.infrastructure.entity.Usuario;
+import com.javanauta.aprendendospring.infrastructure.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+
+    private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping
+    public ResponseEntity<Usuario> salvaUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.ok(usuarioService.salvaUsuario(usuario));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDto usuarioDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDto.getEmail(),
+                        usuarioDto.getSenha())
+        );
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscaUsuarioPorEmail(@RequestParam("email") String email){
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@PathVariable String email){
+        usuarioService.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/javanauta/aprendendospring/controller/dtos/UsuarioDto.java
@@ -0,0 +1,18 @@
+package com.javanauta.aprendendospring.controller.dtos;
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UsuarioDto {
+
+    private String email;
+    private String senha;
+}
